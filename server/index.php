62dabe23a94319574f69e5f463d39ac7aa82cd43
Nishikata Hijiri <416.hijiri.nishikata@gmail.com>
2015-03-23 01:47:27 +0900

    server: a minor change

--- server/index.php
+++ server/index.php
@@ -3,8 +3,19 @@
    Last Modified: 2015-03-19T20:25:11
 -->
 
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+   <meta charset="UTF-8">
+   <title>Sofken Server</title>
+</head>
+
+<body>
 <?php
-   phpinfo();
-/* End of file index.php */
+echo "Sofken Forum Server";
+/* End of file index.php */   
 ?>
+</body>
+
+</html>
